6dd6aac86315e4acd4ac61f6bb73d49b3f19c941
Evan Gamble <solar.fuel@gmail.com>
2015-05-02 11:08:58 -0700

    replace MatchResult with a single long, negative for match failure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject egamble/accessive "1.0.2"
+(defproject egamble/accessive "1.0.3"
   :description "Fast extraction from JSON strings."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [potemkin "0.3.12"]
